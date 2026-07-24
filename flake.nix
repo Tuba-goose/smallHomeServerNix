3201d15da6b1eecde2f2c03da25ee657dd9a186a
Tuba-goose <dmfoster0510@gmail.com>
2026-07-23 20:52:41 -0400

    updated the flake.nix to imports sops

--- flake.nix
+++ flake.nix
@@ -3,14 +3,21 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+		sops-nix.url = "github:Mic92/sops-nix";
+		sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
-	outputs = { self, nixpkgs, ... }: {
+	outputs = { self, nixpkgs, sops-nix, ... }: {
 		nixosConfigurations.HPnixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 
 			modules = [
 				./configuration.nix
+				./dockerStack.nix
+			
+				sops-nix.nixosModules.sops
+				#brings sops into scope for everything else in the config
+				#To edit the secrets.yaml files run sops secrets.yaml
 			];
 		};
 	};
